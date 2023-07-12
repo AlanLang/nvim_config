48a689a9fa9315a381f66a60da55e9d36af60f69
alan <langwdalan@gmail.com>
2023-07-12 11:26:12 -0400

    add rust and lua lsp

--- plugins/community.lua
+++ plugins/community.lua
@@ -1,12 +1,8 @@
 return {
-  -- Add the community repository of plugin specifications
   "AstroNvim/astrocommunity",
-  -- example of imporing a plugin, comment out to use it or add your own
-  -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
-
-  -- { import = "astrocommunity.colorscheme.catppuccin" },
   { import = "astrocommunity.pack.html-css" },
   { import = "astrocommunity.pack.typescript" },
-  { import = "astrocommunity.pack.rust" }
+  { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.pack.lua" }
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
 }
