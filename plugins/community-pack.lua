065e7e7de4d2760ad9ea853ffca1ad83919b12cd
alan <langwdalan@gmail.com>
2023-07-13 09:58:38 -0400

    update community

---
+++ plugins/community-pack.lua
@@ -0,0 +1,12 @@
+return {
+  { import = "astrocommunity.pack.typescript" },
+  { import = "astrocommunity.pack.tailwindcss" },
+  { import = "astrocommunity.pack.markdown" },
+  { import = "astrocommunity.pack.docker" },
+  { import = "astrocommunity.pack.yaml" },
+  { import = "astrocommunity.pack.toml" },
+  { import = "astrocommunity.pack.json" },
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.html-css" },
+  { import = "astrocommunity.pack.rust" },
+}
--- plugins/community.lua
+++ plugins/community.lua
@@ -1,8 +1,4 @@
 return {
   "AstroNvim/astrocommunity",
-  { import = "astrocommunity.pack.html-css" },
-  { import = "astrocommunity.pack.typescript" },
-  { import = "astrocommunity.pack.rust" },
-  { import = "astrocommunity.pack.lua" }
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
 }
