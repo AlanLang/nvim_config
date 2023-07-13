f0a6aa299dc3b0c6b24e95b41c184c7de140b0b0
alan <langwdalan@gmail.com>
2023-07-13 12:14:39 -0400

    update config

---
+++ plugins/community-indent.lua
@@ -0,0 +1,12 @@
+return {
+  { import = "astrocommunity.indent.indent-blankline-nvim" },
+  { import = "astrocommunity.indent.mini-indentscope" },
+  {
+    "echasnovski/mini.indentscope",
+    opts = {
+      symbol = "╎",
+      options = { try_as_border = false, indent_at_cursor = true },
+    },
+  },
+}
+
